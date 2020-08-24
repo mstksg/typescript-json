eaf480906493eb5c277a25f92f20718f0b3b694c
Justin Le <justin@jle.im>
2020-08-24 15:43:39 -0700

    it works a little better i think

--- src/Typescript/Types.hs
+++ src/Typescript/Types.hs
@@ -42,7 +42,7 @@ import           Data.IntMap                               (IntMap)
 import           Data.Kind
 import           Data.Map                                  (Map)
 import           Data.Proxy
-import           Data.SOP                                  (NP(..), NS(..), I(..), K(..))
+import           Data.SOP                                  (NP(..), NS(..), I(..), K(..), (:.:)(..))
 import           Data.Scientific                           (Scientific, toBoundedInteger)
 import           Data.Some                                 (Some(..), withSome, foldSome)
 import           Data.Text                                 (Text)
@@ -54,6 +54,7 @@ import           GHC.OverloadedLabels
 import           GHC.TypeLits
 import qualified Data.Aeson                                as A
 import qualified Data.Aeson.BetterErrors                   as ABE
+import qualified Data.Aeson.Types                          as A
 import qualified Data.SOP                                  as SOP
 import qualified Data.SOP.NP                               as NP
 import qualified Data.Text                                 as T
@@ -94,10 +95,6 @@ data TSPrim :: Type -> Type where
     TSNull       :: TSPrim ()
     TSNever      :: TSPrim Void
 
--- newtype ObjFields as = ObjFields { getObjFields :: NP I as }
-
--- newtype ObjType n as = ObjType { getObjType :: TSType n (ObjFields as) }
-
 type family (as :: [k]) ++ (bs :: [k]) :: [k] where
     '[]     ++ bs = bs
     (a':as) ++ bs = a':(as ++ bs)
@@ -105,10 +102,6 @@ type family (as :: [k]) ++ (bs :: [k]) :: [k] where
 type family Concat (as :: [[k]]) :: [k] where
     Concat '[] = '[]
     Concat (a ': as) = a ++ Concat as
-
-data Iso a b = Iso { iTo :: a -> b, iFrom :: b -> a }
-
--- data ArrayOf ::
 
 data ListOf f a = forall x. ListOf
     (f x)
@@ -145,18 +138,6 @@ withTSType_
     -> r
 withTSType_ f (TSType_ t) = f t
 
--- data KeyList f
-
--- data MapElem :: [Mapping k v] -> k -> v -> Type where
---     MEZ :: MapElem ((k ':-> v) ': kvs) k v
---     MES :: !(MapElem kvs k v) -> MapElem (kv ': kvs) k v
-
--- data PreKeyed ks f a = PreKeyed
---     { pkKey  :: 
---     , pkItem :: f a
---     }
-    
--- type Elem ks a = NS ((:~:) a) ks
 data Elem :: [k] -> k -> Type where
     EZ :: Elem (a ': as) a
     ES :: !(Elem as a) -> Elem (b ': as) a
@@ -211,26 +192,46 @@ testChain = KCCons (,)   id    (Not (\case {})) (Keyed #hello (Identity 10))
           . KCCons const (,()) (Not (\case {})) (Keyed #world (Identity True))
           $ KCNil  ()
 
+data Intersections :: [Symbol] -> Type -> Type -> Type where
+    INil  :: a -> Intersections '[] n a
+    ICons :: (a -> b -> c)
+          -> (c -> (a, b))
+          -> NP (Not :.: Elem kss) ks
+          -> TSType ('Just ks) n a
+          -> Intersections kss n b
+          -> Intersections (ks ++ kss) n c
+
+runCoIntersections
+    :: forall kss n f. Applicative f
+    => (forall ks. TSType ('Just ks) n ~> f)
+    -> Intersections kss n ~> f
+runCoIntersections f = go
+  where
+    go :: Intersections jss n ~> f
+    go = \case
+      INil x -> pure x
+      ICons g _ _ t ts -> liftA2 g (f t) (go ts)
+
+runContraIntersections
+    :: forall kss n f. Divisible f
+    => (forall ks. TSType ('Just ks) n ~> f)
+    -> Intersections kss n ~> f
+runContraIntersections f = go
+  where
+    go :: Intersections jss n ~> f
+    go = \case
+      INil _           -> conquer
+      ICons _ g _ t ts -> divide g (f t) (go ts)
+
+
 data TSType :: Maybe [Symbol] -> Type -> Type -> Type where
     TSArray        :: ListOf (TSType ks n) a -> TSType 'Nothing n a
     TSTuple        :: PreT Ap (TSType_ n) a -> TSType 'Nothing n a
     TSObject       :: KeyChain ks (TSType_ n) a -> TSType ('Just ks) n a
     TSUnion        :: PostT Dec (TSType_ n) a -> TSType 'Nothing n a
     TSNamed        :: n -> TSType ks n a -> TSType ks n a
-    -- ah wait, this is going to be tough.  we want to allow intersections
-    -- where the same type appears in both, but only if they are the same
-    -- type?
-    --
-    -- a type mismatch is a valid type and has no compile error.  but we
-    -- need the a -> Value to be total.  this means maybe we can overwrite
-    -- it with one or the other, if there is more than one thing per field?
-    --
-    -- but the problem is that this needs to be Never in the case that
-    -- there is a type mismatch.  yeah.  so a -> Value has to typecheck,
-    -- but the only way that is possible is if we force a to be Never in
-    -- the case that there is a type mismatch.
-    --
-    -- so either we:
+
+    -- either we:
     --
     -- 1. do not allow duplicates, in which case we disallow some potential
     --    useful types that people would want to use intersections for in
@@ -238,12 +239,10 @@ data TSType :: Maybe [Symbol] -> Type -> Type -> Type where
     -- 2. allow duplicates, but somehow find a way to make sure 'a' is
     --    Void
     --
-    -- #2 seems pretty much impossible :(
-    -- TSIntersection :: KeyChain ks (TSType_ n) a -> TSType ('Just ks) n a
-    -- PreT Ap (TSType ks n) a -> TSType ks n a
-    TSPrimType     :: PS TSPrim a -> TSType ks n a
-
--- data KeyChain :: [Mapping Symbol Type] -> (Type -> Type) -> Type -> Type where
+    -- #2 seems pretty much impossible without a full impelemntation of the
+    -- type system
+    TSIntersection :: Intersections ks n a -> TSType ('Just ks) n a
+    TSPrimType     :: PS TSPrim a -> TSType 'Nothing n a
 
 ppScientific :: Scientific -> PP.Doc x
 ppScientific n = maybe (PP.pretty (show n)) PP.pretty
@@ -281,8 +280,6 @@ ppPrim = \case
 
 data Mapping k v = k :-> v
 
--- data family Sing (a :: k)
-
 data MapElem :: [Mapping k v] -> k -> v -> Type where
     MEZ :: MapElem ((k ':-> v) ': kvs) k v
     MES :: !(MapElem kvs k v) -> MapElem (kv ': kvs) k v
@@ -291,37 +288,6 @@ data MapChange :: [Mapping k v] -> [Mapping k v] -> k -> v -> v -> Type where
     MCZ :: MapChange ((k ':-> v) ': kvs) ((k ':-> u) ': kvs) k v u
     MCS :: !(MapChange kvs kus k v u) -> MapChange (kv ': kvs) (ku ': kus) k v u
 
--- data KeyChain :: [Mapping Symbol Type] -> (Type -> Type) -> Type -> Type where
---     KCNil  :: a -> KeyChain '[] f a
---     KCNew  :: (a -> b -> c)
---            -> (c -> (a, b))
---            -> Not (Some (MapElem ks k))
---            -> Keyed k f a
---            -> KeyChain ks f b
---            -> KeyChain ((k ':-> a) ': ks) f c
---     KCOld  :: (a -> b -> c)
---            -> (c -> (a, b))
---            -> (a -> r -> s)
---            -> (s -> (a, r))
---            -> MapChange ks ks' k b c
---            -> Keyed k f a
---            -> KeyChain ks  f r
---            -> KeyChain ks' f s
-
--- testChain :: KeyChain '[ "hello" ':-> Int, "world" ':-> Bool ] Identity (Int, Bool)
--- testChain = KCNew (,)   id    (Not not1) (Keyed (Identity 10))
---           . KCNew const (,()) (Not not2) (Keyed (Identity True))
---           $ KCNil ()
---   where
---     not1 :: Some (MapElem '[ "world" ':-> Bool ] "hello") -> Void
---     not1 (Some me) = case me of {}
---     not2 :: Some (MapElem '[] "world") -> Void
---     not2 (Some me) = case me of {}
-
--- testChain2 :: KeyChain '[ "hello" ':-> Int, "world" ':-> (String, Bool) ] Identity (Int, (Bool, String))
--- testChain2 = KCOld (,) id (\x (y, z) -> (y, (z, x))) (\(x, (y, z)) -> (z, (x, y))) (MCS MCZ) (Keyed (Identity ("ok" :: String)))
---            $ testChain
-
 ppType
     :: TSType ks Void a
     -> PP.Doc x
@@ -329,12 +295,11 @@ ppType = \case
     TSArray t   -> getConst (interpretListOf (Const . ppType) t) PP.<+> "[]"
     TSTuple ts  -> PP.encloseSep "[" "]" ", " (icollect (withTSType_ ppType) ts)
     TSObject ts -> PP.encloseSep "{" "}" "," . getConst $
-      -- icollect (\(K n :*: x) -> PP.pretty n PP.<+> ": " PP.<+> withTSType_ ppType x) ts
       runCoKeyChain (\k x -> Const [PP.pretty k PP.<+> ": " PP.<+> withTSType_ ppType x]) ts
     TSUnion ts  -> PP.encloseSep "" "" " | " (icollect (withTSType_ ppType) ts)
     TSNamed v _ -> absurd v
-    -- TSIntersection ts  -> PP.encloseSep "" "" " & " (icollect ppType ts)
-    -- TSIntersection ts -> PP.encloseSep "" "" " & " (icollect (ppType . getObjType) ts)
+    TSIntersection ts  -> PP.encloseSep "" "" " & " . getConst $
+      runCoIntersections (\x -> Const [ppType x]) ts
     TSPrimType PS{..} -> ppPrim psItem
 
 enumLitToValue :: EnumLit -> A.Value
@@ -362,6 +327,25 @@ primToValue = \case
     TSNull -> \_ -> A.Null
     TSNever -> absurd
 
+objTypeToValue :: TSType ('Just ks) n a -> a -> [A.Pair]
+objTypeToValue = \case
+    TSObject       ts -> getOp (runContraKeyChain (\k t -> Op $ \x -> [k A..= withTSType_ typeToValue t x]) ts)
+    TSIntersection ts -> getOp (runContraIntersections (Op . objTypeToValue) ts)
+    TSNamed      _ t  -> objTypeToValue t
+
+nonObjTypeToValue
+    :: TSType 'Nothing n a -> a -> A.Value
+nonObjTypeToValue = \case
+    TSArray ts  -> A.Array
+                 . V.fromList
+                 . getOp (interpretListOf (\t -> Op (map (typeToValue t))) ts)
+    TSTuple ts  -> A.Array
+                 . V.fromList
+                 . getOp (preDivisibleT (\t -> Op $ \x -> [withTSType_ typeToValue t x]) ts)
+    TSUnion ts  -> iapply (withTSType_ typeToValue) ts
+    TSNamed _ t -> typeToValue t
+    TSPrimType PS{..} -> primToValue psItem . psSerializer
+
 typeToValue
     :: TSType ks n a -> a -> A.Value
 typeToValue = \case
@@ -375,10 +359,8 @@ typeToValue = \case
                  . getOp (runContraKeyChain (\k t -> Op $ \x -> [k A..= withTSType_ typeToValue t x]) ts)
     TSUnion ts  -> iapply (withTSType_ typeToValue) ts
     TSNamed _ t -> typeToValue t
-    -- hm...
-    -- TSIntersection ts ->
-    --                undefined
-    --              . getOp (preDivisibleT (\t -> Op $ \x -> [typeToValue t x]) ts)
+    TSIntersection ts -> A.object
+                       . getOp (runContraIntersections (Op . objTypeToValue) ts)
     TSPrimType PS{..} -> primToValue psItem . psSerializer
 
 data ParseErr = PEInvalidEnum [(Text, EnumLit)]
@@ -429,8 +411,7 @@ parseType = \case
     TSUnion ts -> foldr @[] (ABE.<|>) (ABE.throwCustomError PENever) $
         icollect (interpretPost (withTSType_ parseType)) (unPostT ts)
     TSNamed _ t -> parseType t
-    -- hm...
-    -- TSIntersection ts -> interpret parseType ts
+    TSIntersection ts -> runCoIntersections parseType ts
     TSPrimType PS{..} -> either (ABE.throwCustomError . PEPrimitive) pure . psParser
                      =<< parsePrim psItem
 
